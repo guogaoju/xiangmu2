6a391384fdbda242036aecb08e9982ab6c006ac2
guogaoju <2248147607@qq.com>
2021-05-28 19:07:55 +0800

    role和dept增删改查接口

--- back-end/server.js
+++ back-end/server.js
@@ -94,6 +94,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/general.routes")(app);
 require("./app/routes/Bumen.routes")(app);
 require("./app/routes/Dept.routes")(app);
+require("./app/routes/Role.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
